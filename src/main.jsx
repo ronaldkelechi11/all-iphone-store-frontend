3026678daf8d84a65fcb5bd60e217fb80d164579
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-07-01 20:37:46 +0100

    upgrade on react-router-dom version

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import './main.css'
 
-import App from './screens/App.jsx'
+import App from './screens/App'
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
