9aed63e0f60ffcdffb3c6d2d67a016a7c256e234
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-07-01 20:24:27 +0100

    Changed the Root folder for main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import App from './screens/App.jsx'
 import './main.css'
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+import App from './screens/App.jsx'
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min'
 
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>)
--- src/screens/App.jsx
+++ src/screens/App.jsx
@@ -1,41 +1,39 @@
 import '../styles/App.scss'
-import { Route, Switch } from 'react-router-dom';
+
+import { BrowserRouter, Router, Route } from "react-router-dom";
 
 import Navbar from '../components/Navbar'
 import Landing from "../components/Landing";
 import Products from '../components/Products';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
-import Signup from '../screens/Signup';
-import LogIn from '../screens/Login';
-import ProductsView from './ProductsView';
-import Admin from './Admin';
+
+import Signup from './Signup';
+import LogIn from './Login';
+import ProductsView from './ProductsView'
+import Admin from './Admin.jsx';
 
 function App() {
-
   function Home() {
-    return (
-      <>
-        <Navbar />
-        <Landing />
-        <Products />
-        <Contact />
-        <Footer />
-      </>
-    )
+    <>
+      <Navbar />
+      <Landing />
+      <Products />
+      <Contact />
+      <Footer />
+    </>
   }
-
   return (
     <>
-      <div className="app">
-        <Switch>
-          <Route exact path="/" > <Home /></Route>
-          <Route path="/signup"> <Signup /></Route>
-          <Route path="/login"> <LogIn /></Route>
-          <Route path="/products"> <ProductsView /></Route>
-          <Route path="/admin"> <Admin /></Route>
-        </Switch>
-      </div>
+      <BrowserRouter>
+        <Router>
+          <Route path="/"> <Home /></Route>
+          <Route exact path="/signup"> <Signup /></Route>
+          <Route exact path="/login"> <LogIn /></Route>
+          <Route exact path="/products"> <ProductsView /></Route>
+          <Route exact path="/admin"> <Admin /></Route>
+        </Router>
+      </BrowserRouter>
     </>
   )
 }
